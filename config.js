85fe2cdc85ade01833827e9196f75879688b1666
zirei <zeatgomez@gmail.com>
2021-10-12 22:34:20 -0500

    update config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://zirei:Kobahi5*@userscluster.uf6mm.mongodb.net/UsersCluster?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://sac_admin:lcxChFg0iXyOppuV@userscluster.uf6mm.mongodb.net/UsersCluster?retryWrites=true&w=majority',
     port: process.env.PORT || 3001,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/app',
