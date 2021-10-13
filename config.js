e0c5a7eb944dc811a732397bd74998b0e2412124
zirei <zeatgomez@gmail.com>
2021-10-12 23:01:08 -0500

    testing

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 const config = {
     dbUrl: process.env.DB_URL || 'mongodb+srv://sac_admin:lcxChFg0iXyOppuV@userscluster.uf6mm.mongodb.net/UsersCluster?retryWrites=true&w=majority',
-    port: process.env.PORT || 3001,
+    port: process.env.PORT || 3004,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/app',
     filesRoute: process.env.FILES_ROUTE || 'files'
